1c0a23555e91430451eea3e4eaf3be3036f186d4
George Zhylin <george.zhylin@gmail.com>
2017-08-22 21:35:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowhile_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWhileTest();
+        }
+        private static void DoWhileTest()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("the value of i = {0}", i);
+                i++;
+            }
+            while (i <= 5);
+        }
+    }
+}
